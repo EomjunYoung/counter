b1675eb14b7d1b14a7c8f65227181cdba4ca4312
EomJunyoung <theoneday737@naver.com>
2021-06-11 16:06:24 +0900

    add onSaveInstanceState

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -12,7 +12,7 @@ import com.example.counter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     //java 1.8버전을 해줘야 됨
-    val viewModel by viewModels<MainViewModel>()
+    private val viewModel by viewModels<MainViewModel>()
 
     private lateinit var binding: ActivityMainBinding
 
@@ -76,5 +76,26 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+    }
+
+    //화면전환될때 보여주기 위해 저장하는 것
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("count", viewModel.count)
+    }
+
+    //onSaveInstanceState에 저장된것을 받는 곳
+    /**
+     *
+     * TODO: Android SDK가 설치된 위치(내 pc: D:\Android\Sdk\platfrom-tools)
+     * adb shell로 들어간 뒤 am force-stop 패키지명(com.example.counter) 를 하면
+     * 에뮬레이터로 실행중인 앱을 죽일수있음
+     *
+     */
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        viewModel.count = savedInstanceState.getInt("count")
     }
 }
