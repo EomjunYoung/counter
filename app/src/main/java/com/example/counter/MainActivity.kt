f5037105b3c3428c2b162cf02e3272d3f38c84fe
EomJunyoung <theoneday737@naver.com>
2021-06-13 10:30:23 +0900

    apply databinding(modify button function and textview)

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.example.counter.databinding.ActivityMainBinding
@@ -28,29 +29,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //binding처럼 해도되고 아래처럼 해도됨(강의는 아래처럼)
+//        val binding2 = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        //View와 xml의 databinding한 것을 연결해주는 부분
+        binding.viewModel = viewModel //얘를안해주면 .xml에 있는 viewModel의 값들이 바인딩 되지 않음
+        binding.lifecycleOwner = this //이 activity의 lifecycle에 맞춰서 동작하겠다는 의미(새로고침 느낌), 안하면 텍스트 안바뀜
+
 //        binding.counterText.text = "${viewModel.count}"
 
         //MutableLiveData를 상속받은 변수가 변할때마다 추적하기 위해 observe(관찰하는 것) 셋팅
-        viewModel.countLiveData.observe(this@MainActivity, Observer {
-            count -> binding.counterText.text = "$count"
-        })
+//        viewModel.countLiveData.observe(this@MainActivity, Observer {
+//            count -> binding.counterText.text = "$count"
+//        })
 
         /** 아래와같이 일일이 viewModel.count를 써서 값이 보여주는것이 아닌
          * 위와같은 LiveData방식을 활용하면 값이 변할때마다 추적하여 값을 자동갱신시켜준다 **/
-        binding.addButton.setOnClickListener {
-//            count++
-//            viewModel.count++
-//            binding.counterText.text = "${viewModel.count}"
-            viewModel.increaseCount()
-        }
+//        binding.addButton.setOnClickListener {
+////            count++
+////            viewModel.count++
+////            binding.counterText.text = "${viewModel.count}"
+//            viewModel.increaseCount()
+//        }
 
-        binding.subButton.setOnClickListener {
-//            count--
-//            viewModel.count--
-//            binding.counterText.text = "${viewModel.count}"
-
-            viewModel.decreaseCount()
-        }
+//        binding.subButton.setOnClickListener {
+////            count--
+////            viewModel.count--
+////            binding.counterText.text = "${viewModel.count}"
+//
+//            viewModel.decreaseCount()
+//        }
 
 
         //화면이 가로세로 바뀔때 호출되는 콜백메소드... 실제로 화면전환되면 로그찍힘
